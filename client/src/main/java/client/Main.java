0a025ea34077b5eaa85e08004e44c1b2f374b890
Olaf Herman <o.h.s.herman@student.tudelft.nl>
2023-03-01 14:33:44 +0100

    Basic board view

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,11 +20,9 @@ import static com.google.inject.Guice.createInjector;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import client.scenes.*;
 import com.google.inject.Injector;
 
-import client.scenes.AddQuoteCtrl;
-import client.scenes.MainCtrl;
-import client.scenes.QuoteOverviewCtrl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -40,10 +38,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
+        var board = FXML.load(BoardView.class, "client", "scenes", "BoardView.fxml");
         var overview = FXML.load(QuoteOverviewCtrl.class, "client", "scenes", "QuoteOverview.fxml");
         var add = FXML.load(AddQuoteCtrl.class, "client", "scenes", "AddQuote.fxml");
 
-        var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
-        mainCtrl.initialize(primaryStage, overview, add);
+        var mainCtrl = INJECTOR.getInstance(BoardMainCtrl.class);
+        mainCtrl.initialize(primaryStage, board);
     }
 }
