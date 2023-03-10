e8a5a44c40e5a78b5a2a511b52a3ce2522d7e2fa
Andrei Petre <a.petre-1@student.tudelft.nl>
2023-03-10 21:38:22 +0100

    Making checkstyle work

--- commons/src/test/java/commons/BoardTest.java
+++ commons/src/test/java/commons/BoardTest.java
@@ -3,9 +3,11 @@ package commons;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BoardTest {
     private Board board1;
