5c1bb046bf3d861d9c5db9a44cc1df9341a3bb4c
Jochem Pouwels <j.j.pouwels@student.tudelft.nl>
2023-03-13 14:30:30 +0100

    Added tests for TaskListController

---
+++ server/src/test/java/server/api/TaskListControllerTest.java
@@ -0,0 +1,132 @@
+package server.api;
+
+import commons.TaskList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public class TaskListControllerTest {
+    private TaskListTestRepository repo;
+    private TaskListController controller;
+
+    @BeforeEach
+    public void setup() {
+        repo = new TaskListTestRepository();
+        controller = new TaskListController(repo);
+    }
+
+    @Test
+    public void cannotAddNullList() {
+        TaskList l = getList("dummy");
+        l.tasks = null;
+        var actual = controller.add(l);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+
+        l = getList("dummy");
+        l.name = null;
+        actual = controller.add(l);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    public void invalidIdTest() {
+        var readResponse = controller.getById(-1);
+        assertEquals(BAD_REQUEST, readResponse.getStatusCode());
+        var deleteResponse = controller.deleteById(-1);
+        assertEquals(BAD_REQUEST, deleteResponse.getStatusCode());
+        var updateResponse = controller.updateById(-1, getList("dummy"));
+        assertEquals(BAD_REQUEST, updateResponse.getStatusCode());
+    }
+
+    @Test
+    public void noListWithIdTest() {
+        var readResponse = controller.getById(10);
+        assertEquals(NOT_FOUND, readResponse.getStatusCode());
+        var deleteResponse = controller.deleteById(10);
+        assertEquals(NOT_FOUND, deleteResponse.getStatusCode());
+        var updateResponse = controller.updateById(10, getList("dummy"));
+        assertEquals(NOT_FOUND, updateResponse.getStatusCode());
+    }
+
+    @Test
+    public void create() {
+        var actual = controller.add(getList("create"));
+        assertEquals(HttpStatus.OK, actual.getStatusCode());
+        assertTrue(actual.hasBody());
+    }
+
+    @Test
+    public void read() {
+        var actual = controller.add(getList("fetch"));
+        if(actual.getBody() == null) return;
+        long id = actual.getBody().id;
+        var actual2 = controller.getById(id);
+        assertEquals(HttpStatus.OK, actual2.getStatusCode());
+        assertEquals(actual.getBody(), actual2.getBody());
+    }
+
+    @Test
+    public void readAll() {
+        var actual = controller.add(getList("List"));
+        if(actual.getBody() == null) return;
+        var actual2 = controller.add(getList("List2"));
+        if(actual2.getBody() == null) return;
+
+        List<TaskList> allLists = controller.getAll();
+        assertEquals(2, allLists.size());
+        assertTrue(allLists.contains(actual.getBody()));
+        assertTrue(allLists.contains(actual2.getBody()));
+    }
+
+    @Test
+    public void update() {
+        TaskList list = getList("update-before");
+        var actual = controller.add(list);
+        if (actual.getBody() == null) return;
+        long id = actual.getBody().id;
+
+        list.name = "update-after";
+        var actual2 = controller.updateById(id, list);
+        assertEquals(HttpStatus.OK, actual2.getStatusCode());
+        assertEquals(actual.getBody(), actual2.getBody());
+    }
+
+    @Test
+    public void updateNull() {
+        var actual = controller.updateById(10, null);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    public void delete() {
+        TaskList list = getList("delete");
+        var actual = controller.add(list);
+        if (actual.getBody() == null) return;
+        long id = actual.getBody().id;
+
+        var actual2 = controller.deleteById(id);
+        assertEquals(HttpStatus.OK, actual2.getStatusCode());
+        assertTrue(repo.calledMethods.contains("deleteById"));
+
+        var actual3 = controller.getById(id);
+        assertNull(actual3.getBody());
+        assertEquals(NOT_FOUND, actual3.getStatusCode());
+    }
+
+    @Test
+    public void databaseIsUsed() {
+        controller.add(getList("l1"));
+        assertTrue(repo.calledMethods.contains("save"));
+    }
+
+    private TaskList getList(String name) {
+        return new TaskList(name, 0);
+    }
+}
---
+++ server/src/test/java/server/api/TaskListTestRepository.java
@@ -0,0 +1,192 @@
+package server.api;
+
+import commons.TaskList;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import server.database.TaskListRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@SuppressWarnings("NullableProblems")
+public class TaskListTestRepository implements TaskListRepository {
+    public final List<TaskList> lists = new ArrayList<>();
+    public final List<String> calledMethods = new ArrayList<>();
+
+    private void call(String name) {
+        calledMethods.add(name);
+    }
+
+    @Override
+    public List<TaskList> findAll() {
+        call("findAll");
+        return lists;
+    }
+
+    @Override
+    public List<TaskList> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<TaskList> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<TaskList> findAllById(Iterable<Long> ids) {
+        call("findAllById");
+        List<TaskList> lists = new ArrayList<>();
+        for (Long id : ids) {
+            Optional<TaskList> taskList = find(id);
+            taskList.ifPresent(lists::add);
+        }
+        return lists;
+    }
+
+    @Override
+    public long count() {
+        return lists.size();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        call("deleteById");
+        find(id).map(lists::remove);
+    }
+
+    @Override
+    public void delete(TaskList entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends TaskList> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends TaskList> S save(S entity) {
+        call("save");
+        entity.id = lists.size();
+        lists.add(entity);
+        return entity;
+    }
+
+    @Override
+    public <S extends TaskList> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<TaskList> findById(Long id) {
+        call("findById");
+        return find(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        call("existsById");
+        return find(id).isPresent();
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends TaskList> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends TaskList> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<TaskList> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public TaskList getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public TaskList getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends TaskList> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends TaskList> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends TaskList> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends TaskList> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends TaskList> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends TaskList> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends TaskList, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+
+    private Optional<TaskList> find(long id) {
+        for (TaskList list : lists) {
+            if (list.id == id) {
+                return Optional.of(list);
+            }
+        }
+
+        return Optional.empty();
+    }
+}
