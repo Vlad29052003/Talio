3e034ec2438c51142db79f74777382a56a939a14
Vlad Graure <graure@student.tudelft.nl>
2023-03-29 18:40:33 +0200

    add javadoc

--- server/src/main/java/server/api/TagController.java
+++ server/src/main/java/server/api/TagController.java
@@ -19,31 +19,65 @@ import java.util.List;
 public class TagController {
     private final TagService tagService;
 
+    /**
+     * Creates a new {@link TagController} object.
+     *
+     * @param tagService is the TagService to be used.
+     */
     @Autowired
     public TagController(TagService tagService) {
         this.tagService = tagService;
     }
 
+    /**
+     * Gets all Tags from the database.
+     *
+     * @return a list of Tags.
+     */
     @GetMapping(path = {"", "/"})
     public List<Tag> getAll() {
         return tagService.getAll();
     }
 
+    /**
+     * Gets a Tag by its id.
+     *
+     * @param id is the id to be used.
+     * @return a response, potentially containing the searched Tag.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable("id") long id) {
         return tagService.getById(id);
     }
 
+    /**
+     * Updates a Tag.
+     *
+     * @param tag is the tag with updated data.
+     * @return a response, potentially containing the updated Tag, if valid.
+     */
     @PostMapping(path = {"", "/"})
     public ResponseEntity<?> update(@RequestBody Tag tag) {
         return tagService.update(tag);
     }
 
+    /**
+     * Creates a Tag.
+     *
+     * @param tag is the new Tag.
+     * @return a response, potentially containing the new Tag, if valid.
+     */
     @PutMapping(path = {"", "/"})
     public ResponseEntity<?> create(@RequestBody Tag tag) {
         return tagService.create(tag);
     }
 
+    /**
+     * Deletes a Tag.
+     *
+     * @param id is the id of the Tag to be deleted.
+     * @return a response containing the status of the action.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") long id) {
         return tagService.delete(id);
--- server/src/main/java/server/service/TagService.java
+++ server/src/main/java/server/service/TagService.java
@@ -12,15 +12,31 @@ import java.util.List;
 public class TagService {
     private final TagRepository tagRepo;
 
+    /**
+     * Creates a new {@link TagService} object.
+     *
+     * @param tagRepo is the TagRepository to be used.
+     */
     @Autowired
     public TagService(TagRepository tagRepo) {
         this.tagRepo = tagRepo;
     }
 
+    /**
+     * Gets all Tags from the database.
+     *
+     * @return a list of Tags.
+     */
     public List<Tag> getAll() {
         return tagRepo.findAll();
     }
 
+    /**
+     * Gets a Tag by its id.
+     *
+     * @param id is the id to be used.
+     * @return a response, potentially containing the searched Tag.
+     */
     public ResponseEntity<?> getById(long id) {
         if(tagRepo.existsById(id)) {
             return ResponseEntity.ok(tagRepo.findById(id).get());
@@ -28,6 +44,12 @@ public class TagService {
         return ResponseEntity.badRequest().build();
     }
 
+    /**
+     * Updates a Tag.
+     *
+     * @param tag is the tag with updated data.
+     * @return a response, potentially containing the updated Tag, if valid.
+     */
     public ResponseEntity<?> update(Tag tag) {
         if(tagRepo.existsById(tag.id)) {
             Tag updated = tagRepo.findById(tag.id).get();
@@ -38,12 +60,24 @@ public class TagService {
         return ResponseEntity.badRequest().build();
     }
 
+    /**
+     * Creates a Tag.
+     *
+     * @param tag is the new Tag.
+     * @return a response, potentially containing the new Tag, if valid.
+     */
     public ResponseEntity<?> create(Tag tag) {
         if(tag.board == null)return ResponseEntity.badRequest().build();
         Tag saved = tagRepo.saveAndFlush(tag);
         return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Deletes a Tag.
+     *
+     * @param id is the id of the Tag to be deleted.
+     * @return a response containing the status of the action.
+     */
     @Transactional
     public ResponseEntity<?> delete(long id) {
         if(tagRepo.existsById(id)) {
