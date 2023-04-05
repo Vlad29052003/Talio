eed917e0f155bb55bcb0c626f57db639e7288c94
Jochem Pouwels <j.j.pouwels@student.tudelft.nl>
2023-04-05 14:27:12 +0200

    added button to add subtask

--- client/src/main/java/client/scenes/crud/task/OpenTaskCtrl.java
+++ client/src/main/java/client/scenes/crud/task/OpenTaskCtrl.java
@@ -1,9 +1,22 @@
 package client.scenes.crud.task;
 
 import commons.Task;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.Optional;
 
 public class OpenTaskCtrl {
     private Task task;
+    @FXML
+    private Label name;
+    @FXML
+    private TextArea description;
+    @FXML
+    private ProgressBar progress;
+    @FXML
+    private ListView<String> subTasks;
 
     /**
      *
@@ -13,5 +26,34 @@ public class OpenTaskCtrl {
      */
     public void setTask(Task task) {
         this.task = task;
+        refresh();
+    }
+
+    /**
+     * Refreshes the scene.
+     */
+    public void refresh() {
+        name.setText(task.name);
+        description.setText(task.description);
+        progress.setProgress(0);
+        subTasks.setItems(FXCollections.observableArrayList(task.subtasks));
+        subTasks.refresh();
+    }
+
+    /**
+     *
+     * Adds a SubTask when the button is clicked.
+     *
+     */
+    public void onAddSubTask() {
+        TextInputDialog dialog = new TextInputDialog("sub task name");
+        dialog.setTitle("Sub task name");
+        dialog.setHeaderText(null);
+        dialog.setGraphic(null);
+        dialog.setContentText("please enter the sub task text: ");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> task.addSubTask(name));
+        refresh();
     }
 }
